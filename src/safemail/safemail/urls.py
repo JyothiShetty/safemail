9548920fc734d528327f11f4c43de18020289ccd
Jyothi B R <jyothibr42@gmail.com>
2021-04-14 22:52:54 +0530

    Update urls.py

--- src/safemail/safemail/urls.py
+++ src/safemail/safemail/urls.py
@@ -18,6 +18,9 @@ from django.urls import path ,include
 from django.views.generic.base import TemplateView
 from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'),{'form_class': 'core/login.html'},),
